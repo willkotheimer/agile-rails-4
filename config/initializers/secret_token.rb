4f0d6b2f58fca4b76080fe2537007df526a02850
Will Kotheimer <wkotheimer@gmail.com>
2014-08-08 20:58:57 -0400

    Initialize repo for Agile Book

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = 'fac0bd729d0bf60314d855647b2a178be2b45656f851f9849a4cd230b934bebb7c8677754b3e9fbc4d115b31d3bd400dfb5c66fe5c28e51e382f5b1b90770040'
